447a4487950e662ea5c57057c4ef81be54aa3827
Nicholas Fordyce <nfordyc@purdue.edu>
2019-09-10 13:08:57 -0400

    Create basic header component

---
+++ frontend/src/components/HeaderBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function HeaderBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <ToolBar>
+          <Typography variant="h6" className={classes.title}>
+            WhenToMeet
+          </Typography>
+        </ToolBar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default HeaderBar;
