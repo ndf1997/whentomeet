c1ce62949f9dbade41b3f0b10fa7f31a0f4c17f3
Nicholas Fordyce <nfordyc@purdue.edu>
2019-09-16 19:51:08 -0400

    Add styling to tooltip

--- frontend/src/components/TimeTableTooltip.tsx
+++ frontend/src/components/TimeTableTooltip.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import PropTypes, { InferProps } from 'prop-types';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 
 import { Member } from '../types/Member';
 
+const useStyles = makeStyles(() => createStyles({
+  text: {
+    fontSize: 14,
+  }
+}));
+
 function TimeTableTooltip (props: InferProps<typeof TimeTableTooltip.propTypes>) {
   const { children, day, time, members, percentage } = props;
+
+  const classes = useStyles();
+
   return (
     <Tooltip placement="top" title={
       <React.Fragment>
-        <Typography variant="h6" gutterBottom>{`${day}, ${time}`}</Typography>
-        <Typography gutterBottom>{`Availability: ${percentage}`}</Typography>
-        {members.map((member: Member) => <Typography>{member.name}</Typography>)}
+        <Typography className={classes.text} align="center" variant="h6" gutterBottom>
+          {`${day}, ${time}`}
+        </Typography>
+        <Typography className={classes.text} align="center" gutterBottom>
+          {`Availability: ${percentage}`}
+        </Typography>
+        {members.map((member: Member) => (
+          <Typography className={classes.text} align="center">
+            {member.name}
+          </Typography>))
+        }
       </React.Fragment>
       }
     >
