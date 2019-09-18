fd88109376cd6ccf3b6a59078502a4cc4c6741f6
Nicholas Fordyce <nfordyc@purdue.edu>
2019-09-18 10:57:15 -0400

    Set up paths for home page and meeting page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import MeetingPage from './pages/meeting/MeetingPage';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={HomePage} />
+        <Route path="/meeting/:meetingId" component={MeetingPage} />
+      </div>
+    </Router>
   );
 }
 
