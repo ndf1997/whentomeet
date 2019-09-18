214e9927496833fc220266e75f47b4fde3e3200e
Nicholas Fordyce <nfordyc@purdue.edu>
2019-09-18 11:40:42 -0400

    Add path for meeting form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import MeetingPage from './pages/meeting/MeetingPage';
+import MeetingForm from './pages/meetingform/MeetingForm';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/create" component={MeetingForm} />
         <Route path="/meeting/:meetingId" component={MeetingPage} />
       </div>
     </Router>
