f41b0e4b97a5d8422dd7a67698f565d096ea1fe0
Nicholas Fordyce <nfordyc@purdue.edu>
2019-09-18 20:46:38 -0400

    WIP axios get call

--- frontend/src/pages/meeting/MeetingPage.tsx
+++ frontend/src/pages/meeting/MeetingPage.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, RouteComponentProps } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import HeaderBar from '../../components/HeaderBar';
 import MeetingDetails from '../../components/MeetingDetails';
 import TimeTable from '../../components/TimeTable';
 
-function MeetingPage() {
+import { serverURL } from '../../types/constants';
+
+type TParams =  { meetingId: string };
+
+function MeetingPage({ match }: RouteComponentProps<TParams>) {
+  function componentDidMount() {
+    const server = axios.create({
+      baseURL: serverURL,
+    });
+
+    const meetingId = match.params.meetingId;
+
+    if (typeof meetingId !== 'undefined') {
+      server.get('/meeting/' + meetingId)
+        .then(response => {
+          console.log(response);
+        })
+    }
+  }
+  useEffect(() => componentDidMount(), [])
+
   return (
     <div className="MeetingPage">
       <HeaderBar />
