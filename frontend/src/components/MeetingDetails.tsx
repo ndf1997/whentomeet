6464a21a6015113dc4504532e61d79fc9db84e3f
ndf1997 <ndf1997@gmail.com>
2019-09-26 18:50:18 -0400

    Edit Meeting Added invite url to meeting details Added Edit Meeting option

--- frontend/src/components/MeetingDetails.tsx
+++ frontend/src/components/MeetingDetails.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { InferProps } from 'prop-types';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-
+import Button from '@material-ui/core/Button';
 import { MeetingPropType } from '../types/Meeting';
+import EditMeeting from './EditMeeting';
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   root: {
@@ -18,23 +19,58 @@ function MeetingDetails(props: InferProps<typeof MeetingDetails.propTypes>) {
   const { meeting } = props;
   const classes = useStyles();
   const isSelected = meeting.selectedTime !== '';
-
+  const [title, setTitle] = React.useState(meeting.title);
+  const [location, setLocation] = React.useState(meeting.location);
+  const [description, setDescription] = React.useState(meeting.description);
+  const [open, setOpen] = React.useState(false);
+  function titleHandler(titleEdit: string) {
+    setTitle(titleEdit);
+  }
+    
+  function locationHandler(locationEdit: string) {
+    setLocation(locationEdit);
+  }
+  function descriptionHandler(descriptionEdit: string) {
+    setDescription(descriptionEdit);
+  }
+  function editHandler() {
+    setOpen(false);
+  }
+  function meetingEdit() {
+    setOpen(true);
+  }
   return (
     <div className={classes.root}>
+      <EditMeeting 
+    meeting_id={meeting.meeting_id}
+    titleHandler={titleHandler} 
+    descriptionHandler={descriptionHandler}
+    locationHandler={locationHandler}
+    open={open}
+    editHandler={editHandler}
+    existingTitle={title}
+    existingDescription={description}
+    existingLocation={location}/>
       <Typography variant="h3" gutterBottom >
-        {meeting.title}
+        {title}
       </Typography>
       <Typography variant="body1" className={isSelected ? '' : classes.description} >
-        {meeting.description}
+        {description}
       </Typography>
       <Typography variant="body1">
-        Location: {meeting.location}
+        Location: {location}
+      </Typography>
+      <Typography variant="body1">
+        Invite URL: {meeting.url}
       </Typography>
       {isSelected &&
         <Typography variant="body1">
           Meeting Time: {meeting.selectedTime}
         </Typography>
       }
+      <Button variant="outlined" onClick={meetingEdit}>
+        Edit Meeting
+      </Button>
     </div>
   );
 }
