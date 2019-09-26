94ce29927088a3f3e2cfeb7049c78fb123e50a66
ndf1997 <ndf1997@gmail.com>
2019-09-26 02:42:17 -0400

    Added backend functionality to MeetingForm

--- frontend/src/pages/meetingform/MeetingForm.tsx
+++ frontend/src/pages/meetingform/MeetingForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AddTitle from '../../components/AddTitle';
 import AddDescription from '../../components/AddDescription';
@@ -8,6 +9,9 @@ import {Redirect} from 'react-router-dom';
 import HeaderBar from '../../components/HeaderBar';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import {History} from 'history';
+import { serverURL } from '../../types/constants';
+import PropTypes, {InferProps} from 'prop-types';
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   root: {
@@ -28,27 +32,62 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
   }
 }));
 
-function MeetingForm() {
+interface MeetingFormProps {
+  history: History
+}
+
+function MeetingForm(props: MeetingFormProps) {
+  
   const [title, setTitle] = React.useState();
   const [redirect, setRedirect] = React.useState(false);
   const [location, setLocation] = React.useState();
   const [description, setDescription] = React.useState();
+  const [meetingId, setMeetingId] = React.useState();
+  const [meetingPage, setMeetingPage] = React.useState();
+  const server = axios.create({
+    baseURL: serverURL,
+  });
+
+  function createNewMeeting() {
+    const newMeeting = {
+      meeting_id: '1',
+      title: title,
+      description: description,
+      location: location,
+      members: [],
+    };
+    server.post('/meeting', JSON.stringify(newMeeting))
+      .then(response => {
+        const meeting_id: string = response.data.meeting_id;
+        console.log(response);
+        console.log(response.data.meeting_id);
+        setMeetingId(meeting_id);
+      });
+  }
+
   function titleHandler(event: React.ChangeEvent<HTMLInputElement>) {
     setTitle(event.target.value);
   }
+
   function locationHandler(event: React.ChangeEvent<HTMLInputElement>) {
     setLocation(event.target.value);
   }
+
   function descriptionHandler(event: React.ChangeEvent<HTMLInputElement>) {
     setDescription(event.target.value);
   }
+
+
   function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    createNewMeeting();
+    setMeetingPage('/meeting/'+meetingId);
     setRedirect(true);
+    redirectHandler();
   }
   function redirectHandler() {
     if (redirect) {
-      return <Redirect to='/meeting/1234'/>;
+      props.history.push('/meeting/'+meetingId);
     }
   }
   
@@ -56,7 +95,6 @@ function MeetingForm() {
   return (
     <div className="MeetingForm">
     <HeaderBar />
-    {redirectHandler()}
     <form onSubmit={onSubmit}>
         <div className={classes.root}>
           <Typography variant="h3" gutterBottom >
