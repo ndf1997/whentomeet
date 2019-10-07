5893ee67116ac7fe7b7e3624ca0d9ff6f0289c65
Nicholas Fordyce <nfordyc@purdue.edu>
2019-10-07 17:34:33 -0400

    Add UploadFile Button

--- frontend/src/components/UploadFiles.tsx
+++ frontend/src/components/UploadFiles.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import PropTypes, { InferProps } from 'prop-types';
-import { Dialog, DialogTitle } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { Dialog, DialogTitle, DialogContent } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  addIcon: {
+    marginRight: theme.spacing(2),
+  }
+}));
 
 function UploadFiles(props: InferProps<typeof UploadFiles.propTypes>) {
+  const classes = useStyles();
+
   return (
     <Dialog
       onClose={() => props.closeFileDialog()}
       open={props.open}
-      maxWidth="xl"
+      maxWidth="lg"
       fullWidth
     >
       <DialogTitle>FileManager</DialogTitle>
+      <DialogContent>
+        <Button
+          variant="contained"
+          color="primary"
+        >
+          <AddIcon className={classes.addIcon}/>
+          Upload File
+        </Button>
+      </DialogContent>
     </Dialog>
   )
 }
