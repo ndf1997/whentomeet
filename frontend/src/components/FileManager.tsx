c9be04222c165544d3bba5d8b8658f3032800bb0
Nicholas Fordyce <nfordyc@purdue.edu>
2019-10-27 20:34:10 -0400

    Revert "File manager cancel does nothing"
    This reverts commit e4229b081f6d2e8b201c3acfa236612107db10be.

--- frontend/src/components/FileManager.tsx
+++ frontend/src/components/FileManager.tsx
@@ -47,7 +47,7 @@ function FileManager(props: InferProps<typeof FileManager.propTypes>) {
         <FileList files={files} deleteFile={deleteFile} />
       </DialogContent>
       <DialogActions>
-        <Button>
+        <Button onClick={() => closeFileDialog()}>
           Cancel
         </Button>
         <UploadFiles meetingId={meetingId} getFiles={getFiles} />
